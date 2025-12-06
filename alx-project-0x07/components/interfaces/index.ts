d5d2c4180c3cf5b7502f15eb8ae4b6420342bfaa
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-12-06 12:58:00 +0100

    index.ts edited

--- alx-project-0x07/components/interfaces/index.ts
+++ alx-project-0x07/components/interfaces/index.ts
@@ -3,3 +3,15 @@ import * as React from "react"
 export interface ReactComponentProps {
     children: React.ReactNode
 }
+
+export interface LayoutProps {
+    children: React.ReactNode;
+}
+
+export interface HeaderProps {
+    title: string;
+}
+
+export interface FooterProps {
+    year: number;
+}
